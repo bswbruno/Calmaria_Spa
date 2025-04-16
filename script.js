0d7b0bbc843ba7b4989d60c640c120b9e44dbfa4
Bruno Santos <brunusuxx@gmail.com>
2025-04-16 20:32:37 -0300

    adicionado efeitos js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const elementos = document.querySelectorAll(
+    "section, .container__cards--card"
+  );
 
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("aparecer");
+        } else {
+          entry.target.classList.remove("aparecer"); // Remove o efeito ao sair da tela
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  elementos.forEach((el) => {
+    el.classList.add("escondido");
+    observer.observe(el);
+  });
+});
